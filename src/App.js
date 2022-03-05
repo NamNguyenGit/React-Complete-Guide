4e69b7d9e2c26adef3695e33bc7e3dea8bf94346
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-05 19:15:56 +0700

    Create Context

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
-
-  const [cartIsShown,setCartIsShown] = useState(false)
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    setCartIsShown(true)
-  }
+    setCartIsShown(true);
+  };
 
   const hideCartHandler = () => {
-    setCartIsShown(false)
-  }
-  
+    setCartIsShown(false);
+  };
+
   return (
-    <div className="App">
-     { cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </div>
+   
+      <CartProvider>
+        {cartIsShown && <Cart onClose={hideCartHandler} />}
+        <Header onShowCart={showCartHandler} />
+        <main>
+          <Meals />
+        </main>
+      </CartProvider>
+   
   );
 }
 
