19efdccfcd4abc0634f266ab700a88c0e260b25e
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-07 10:31:39 +0700

    Remove Item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,18 +11,24 @@ const Cart = ({ onClose }) => {
 
   const hasItem = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
-          item={item}
-          onAdd={cartItemAddHandler.bind(null,item)}
-          onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onAdd={cartItemAddHandler.bind(null, item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
         />
       ))}
     </ul>
