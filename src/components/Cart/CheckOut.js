ea467ba37460ee5014c33ae863c8654cbcee8a38
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-09 10:44:39 +0700

    Validate Usign Ref

--- src/components/Cart/CheckOut.js
+++ src/components/Cart/CheckOut.js
@@ -1,34 +1,47 @@
 import classes from "./CheckOut.module.css";
+import { useRef } from "react";
 
 const CheckOut = (props) => {
+  const nameInput = useRef();
+  const streetInput = useRef();
+  const postalCodeInput = useRef();
+  const cityInput = useRef();
 
-    const confirmHandler = (e) => {
-        e.preventDefault();
-        
+  const confirmHandler = (e) => {
+    e.preventDefault();
 
-    }
+    const entedredName = nameInput.current.value;
+    const entedredStreet = streetInput.current.value;
+    const entedredPostalCode = postalCodeInput.current.value;
+    const entedredCity = cityInput.current.value;
+
+  };
 
   return (
     <>
       <form onSubmit={confirmHandler}>
         <div className={classes.control}>
           <label htmlFor="name">Your Name</label>
-          <input type="text" id="name" />
+          <input ref={nameInput} type="text" id="name" />
         </div>
         <div className={classes.control}>
           <label htmlFor="street">Your Street</label>
-          <input type="text" id="street" />
+          <input ref={streetInput} type="text" id="street" />
         </div>
         <div className={classes.control}>
           <label htmlFor="postal">Postal Code</label>
-          <input type="text" id="postal" />
+          <input ref={postalCodeInput} type="text" id="postal" />
         </div>
         <div className={classes.control}>
           <label htmlFor="city">Your City</label>
-          <input type="text" id="city" />
+          <input ref={cityInput} type="text" id="city" />
         </div>
-        <button type="button" onClick={props.onCancel}>Cancel</button>
-        <button>Confirm</button>
+        <div className={classes.actions}>
+          <button type="button" onClick={props.onCancel}>
+            Cancel
+          </button>
+          <button className={classes.submit}>Confirm</button>
+        </div>
       </form>
     </>
   );
